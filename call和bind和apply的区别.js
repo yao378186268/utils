fc028a05d5a8207bcf7a3c9ee5c56e7dbca78458
姚成成 <378186268@qq.com>
2022-01-06 11:47:15 +0800

    call、bind和apply的区别

---
+++ call和bind和apply的区别.js
@@ -0,0 +1,8 @@
+使用场景：在methods方法中改变this为thisObject
+
+
+call：methods.call(thisObject,prams1,params2....)
+
+bind；methods.bind(thisObject,parmas1,params2...)()  || methods.bind(thisObject)(parmas1,params2...)
+
+apply：methods.apply(thisObject,[parmas1,params2...])
